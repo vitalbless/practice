0f304a1cb50a2009fe59bddcdbd8bba50bbce5b1
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-01-12 12:11:26 +0300

    полноценно настроил сайт

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Hello Sass</h1>
+    <h2 class="text-4xl">Hello Tailwind</h2>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Practice</title>
+    <title>@yield('title', 'Practice')</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    @vite(['resources/css/app.css', 'resources/js/app/js'])
+    @yield('style')
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
-
+    <main>
+        @include('include.header')
+        @yield('content')
+        @include('include.footer')
+    </main>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 </body>
 
 </html>
