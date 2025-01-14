b6bf25780b173be2800a2b09caa79e56206b2f2d
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-01-14 20:42:57 +0300

    добавил display contents to overlay и лого изменил

--- resources/views/features/header/logo/logo.blade.php
+++ resources/views/features/header/logo/logo.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ route('home') }}" title="Home" aria-label="Home" class="header__logo logo">
-    <img src="{{ asset('icons/header/header.svg') }}" alt="" class="logo__image" width="179" height="50" />
+    <img src="{{ asset('icons/header/logo-header.svg') }}" alt="" class="logo__image" />
 </a>
