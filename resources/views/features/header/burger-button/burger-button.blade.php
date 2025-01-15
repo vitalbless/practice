07867e337eace3c7d044a6687b3f37bfeb7d7af6
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-01-15 20:34:53 +0300

    сверстал кнопку и адаптив

--- resources/views/features/header/burger-button/burger-button.blade.php
+++ resources/views/features/header/burger-button/burger-button.blade.php
@@ -1,4 +1,5 @@
-<button class="header__burger-button burger-button visible-mobile" type="button" aria-label="Open menu" title="Open menu">
+<button class="header__burger-button burger-button is-active visible-mobile" type="button" aria-label="Open menu"
+    title="Open menu">
     <span class="burger-button__line"></span>
     <span class="burger-button__line"></span>
     <span class="burger-button__line"></span>
