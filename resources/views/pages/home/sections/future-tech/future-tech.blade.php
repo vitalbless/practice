428abf2ae6dce962aa010670e6ea8eba56d80d11
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-01-20 13:41:34 +0300

    стили для tag

--- resources/views/pages/home/sections/future-tech/future-tech.blade.php
+++ resources/views/pages/home/sections/future-tech/future-tech.blade.php
@@ -1,4 +1,4 @@
 <section class="section">
-    @include('pages.home.sections.future-tech.components.future-body')
     @include('pages.home.sections.future-tech.components.future-header')
+    @include('pages.home.sections.future-tech.components.future-body')
 </section>
