66e660863627a58d80f81cf77d2da510f70f578b
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-01-20 11:58:36 +0300

    доделал адаптивный дизайн к блоку hero

--- resources/views/pages/home/sections/hero/components/hero-body.blade.php
+++ resources/views/pages/home/sections/hero/components/hero-body.blade.php
@@ -1,6 +1,6 @@
 <div class="hero__body">
     <p class="hero__subtitle">Your Journey to Tomorrow Begins Here</p>
-    <h1 class="hero__title" id="hero-title">Explore the Frontiers of Artificial Intelligence</h1>
+    <h1 class="hero__title h1" id="hero-title">Explore the Frontiers of Artificial Intelligence</h1>
     <div class="hero__description">
         <p>Welcome to the epicenter of AI innovation. FutureTech AI News is your passport
             to a world where machines think, learn, and reshape the future. Join us on this visionary expedition
