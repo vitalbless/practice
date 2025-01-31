6a27852a18c603aab1c98f2ff94709651fc7c15c
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-01-31 20:32:09 +0300

    адаптировал video player и начал делать js логику для работы с видео

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,11 @@ import "./bootstrap";
 import Header from "./header/burger-button";
 import { createMenu } from "./header/menu";
 import TabsCollection from "./blog/tabs";
+import VideoPlayerCollection from "./news/video-player";
 
 new Header();
 new TabsCollection();
+new VideoPlayerCollection();
 
 document.addEventListener("DOMContentLoaded", function () {
     const HeaderMenu = createMenu({
