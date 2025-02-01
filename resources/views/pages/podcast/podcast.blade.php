b6b10226f99bb93fb6a34a207dad75caca03b96a
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-02-01 11:28:30 +0300

    доделал страницу podcast

--- resources/views/pages/podcast/podcast.blade.php
+++ resources/views/pages/podcast/podcast.blade.php
@@ -3,4 +3,6 @@
 @section('title', 'Podcast')
 
 @section('content')
+    @include('pages.podcast.sections.podcasts-title')
+    @include('pages.podcast.sections.podcasts-videos')
 @endsection
---
+++ resources/views/pages/podcast/sections/podcasts-title.blade.php
@@ -0,0 +1,167 @@
+<section class="hero-alt" aria-labelledby="podcasts-title">
+    <header class="hero-alt__header">
+        <div class="hero-alt__header-inner container">
+            <h1 class="hero-alt__title" id="podcasts-title">
+                Unlock the World of Artificial Intelligence <span class="hero-alt__title-hidden-part">through
+                    Podcasts</span>
+            </h1>
+            <p class="hero-alt__subtitle h1 hidden-mobile" aria-hidden="true">
+                through Podcasts
+            </p>
+            <p class="hero-alt__description">Dive deep into the AI universe with our collection of insightful podcasts.
+                Explore the latest trends, breakthroughs, and discussions on artificial intelligence. Whether you're an
+                enthusiast or a professional, our AI podcasts offer a gateway to knowledge and innovation.</p>
+        </div>
+    </header>
+    <div class="hero-alt__body">
+        <ul class="list">
+            <li class="list__item">
+                <div class="card container">
+                    <div class="card__preview">
+                        <div class="card__preview-main">
+                            <img class="card__preview-icon" src="{{ asset('icons/podcasts/icon-1.svg') }}"
+                                alt="" width="80" height="80" loading="lazy" />
+                            <div class="card__preview-info">
+                                <h2 class="card__preview-title h3">AI Revolution</h2>
+                                <div class="card__rating-view rating-view" aria-label="Rating 5 star"
+                                    title="Rating 5 star">
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card__preview-extra">
+                            <div class="card__cell tile">
+                                <h3 class="card__cell-subtitle">Host</h3>
+                                <p class="card__cell-description h6">Dr. Sarah Mitchell</p>
+                                <a class="card__cell-link button" href="/">
+                                    <span class="icon icon--yellow-arrow">Listen Podcast</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card__body">
+                        <div class="card__grid card__grid--3-cols">
+                            <div class="card__cell card__cell--wide">
+                                <div class="video-player" data-js-video-player>
+                                    <video class="video-player__video" width="917" height="412"
+                                        src="{{ asset('videos/example.mp4') }}"
+                                        poster="{{ asset('icons/videos/poster-1.jpg') }}"
+                                        data-js-video-player-video></video>
+                                    <div class="video-player__panel video-player__panel--center is-active"
+                                        data-js-video-player-panel>
+                                        <button class="video-player__play-button" type="button" aria-label="Play video"
+                                            title="Play video" data-js-video-player-play-button>
+                                            <svg width="58" height="58" viewBox="0 0 58 58" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                                    d="M0.5625 29C0.5625 13.2944 13.2944 0.5625 29 0.5625C44.7056 0.5625 57.4375 13.2944 57.4375 29C57.4375 44.7056 44.7056 57.4375 29 57.4375C13.2944 57.4375 0.5625 44.7056 0.5625 29ZM41.4663 26.1318C43.7167 27.382 43.7167 30.6183 41.4663 31.8685L25.1248 40.9471C22.9377 42.1621 20.25 40.5807 20.25 38.0788V19.9215C20.25 17.4196 22.9377 15.8381 25.1248 17.0531L41.4663 26.1318Z"
+                                                    fill="white" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card__cell card__cell--wide">
+                                <h3 class="card__cell-title h4">Delves into the transformative impact of AI</h3>
+                                <p class="card__cell-description">Join Dr. Sarah Mitchell as she delves into the
+                                    transformative impact of AI on industries, featuring expert interviews and
+                                    real-world case studies. Explore the possibilities of AI in healthcare, finance, and
+                                    more.</p>
+                            </div>
+                            <div class="card__cell tile">
+                                <p class="card__cell-title">Total Episodes</p>
+                                <p class="card__cell-description h6">50</p>
+                            </div>
+                            <div class="card__cell tile">
+                                <p class="card__cell-title">Average Episode Length</p>
+                                <p class="card__cell-description h6">30 min</p>
+                            </div>
+                            <div class="card__cell tile">
+                                <p class="card__cell-title">Release Frequency</p>
+                                <p class="card__cell-description h6">Weekly</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="list__item">
+                <div class="card container">
+                    <div class="card__preview">
+                        <div class="card__preview-main">
+                            <img class="card__preview-icon" src="{{ asset('icons/podcasts/icon-2.svg') }}"
+                                alt="" width="80" height="80" loading="lazy" />
+                            <div class="card__preview-info">
+                                <h2 class="card__preview-title h3">AI Revolution</h2>
+                                <div class="card__rating-view rating-view" aria-label="Rating 5 star"
+                                    title="Rating 5 star">
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                    <div class="rating-view__star is-active"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card__preview-extra">
+                            <div class="card__cell tile">
+                                <h3 class="card__cell-subtitle">Host</h3>
+                                <p class="card__cell-description h6">Dr. Sarah Mitchell</p>
+                                <a class="card__cell-link button" href="/">
+                                    <span class="icon icon--yellow-arrow">Listen Podcast</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card__body">
+                        <div class="card__grid card__grid--3-cols">
+                            <div class="card__cell card__cell--wide">
+                                <div class="video-player" data-js-video-player>
+                                    <video class="video-player__video" width="917"
+                                        src="{{ asset('videos/example.mp4') }}"
+                                        poster="{{ asset('icons/videos/poster-1.jpg') }}" height="412"
+                                        data-js-video-player-video></video>
+                                    <div class="video-player__panel video-player__panel--center is-active"
+                                        data-js-video-player-panel>
+                                        <button class="video-player__play-button" type="button"
+                                            aria-label="Play video" title="Play video"
+                                            data-js-video-player-play-button>
+                                            <svg width="58" height="58" viewBox="0 0 58 58" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                                    d="M0.5625 29C0.5625 13.2944 13.2944 0.5625 29 0.5625C44.7056 0.5625 57.4375 13.2944 57.4375 29C57.4375 44.7056 44.7056 57.4375 29 57.4375C13.2944 57.4375 0.5625 44.7056 0.5625 29ZM41.4663 26.1318C43.7167 27.382 43.7167 30.6183 41.4663 31.8685L25.1248 40.9471C22.9377 42.1621 20.25 40.5807 20.25 38.0788V19.9215C20.25 17.4196 22.9377 15.8381 25.1248 17.0531L41.4663 26.1318Z"
+                                                    fill="white" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card__cell card__cell--wide">
+                                <h3 class="card__cell-title h4">Delves into the transformative impact of AI</h3>
+                                <p class="card__cell-description">Join Dr. Sarah Mitchell as she delves into the
+                                    transformative impact of AI on industries, featuring expert interviews and
+                                    real-world case studies. Explore the possibilities of AI in healthcare, finance, and
+                                    more.</p>
+                            </div>
+                            <div class="card__cell tile">
+                                <p class="card__cell-title">Total Episodes</p>
+                                <p class="card__cell-description h6">50</p>
+                            </div>
+                            <div class="card__cell tile">
+                                <p class="card__cell-title">Average Episode Length</p>
+                                <p class="card__cell-description h6">30 min</p>
+                            </div>
+                            <div class="card__cell tile">
+                                <p class="card__cell-title">Release Frequency</p>
+                                <p class="card__cell-description h6">Weekly</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
